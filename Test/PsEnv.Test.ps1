d7541174cee30a4a96176bdfd506fb56ee4bed8e
Kier Dugan <kjd1v07@ecs.soton.ac.uk>
2013-10-22 23:28:42 +0100

    Rewrote the previous unit tests to correctly make use of $ActualValue and added a test for 'defer'.

--- Test/PsEnv.Test.ps1
+++ Test/PsEnv.Test.ps1
@@ -29,8 +29,11 @@ function Test.Use-Tool_FooAndBarVariablesCreated_FooAndBarNullified() {
     Use-Tool -ToolName test -ToolSpec 'set-test'
 
     ## Assert
-    Assert-That -ActualValue $env:path -Constraint {
-        (($env:FOO -eq 'Hello') -and ($env:BAR -eq 'World!'))
+    Assert-That -ActualValue $env:FOO -Constraint {
+        $ActualValue -eq 'Hello'
+    }
+    Assert-That -ActualValue $env:BAR -Constraint {
+        $ActualValue -eq 'World!'
     }
 }
 
@@ -45,11 +48,17 @@ function Test.Use-Tool_FooAndBarEqualToHelloWorld_FooAndBarHelloAndWorld()
     Use-Tool -ToolName test -ToolSpec 'modify-test'
 
     ## Assert
-    Assert-That -ActualValue $env:path -Constraint {
-        (($env:FOO -eq 'Hello, World!') -and
-            ($env:BAR -eq 'Hello, World!') -and
-            ($env:BAZ -eq 'appended') -and
-            ($env:FOT -eq 'prepended'))
+    Assert-That -ActualValue $env:FOO -Constraint {
+        $ActualValue -eq 'Hello, World!'
+    }
+    Assert-That -ActualValue $env:BAR -Constraint {
+        $ActualValue -eq 'Hello, World!'
+    }
+    Assert-That -ActualValue $env:BAZ -Constraint {
+        $ActualValue -eq 'appended'
+    }
+    Assert-That -ActualValue $env:FOT -Constraint {
+        $ActualValue -eq 'prepended'
     }
 }
 
@@ -62,7 +71,29 @@ function Test.Use-Tool_BazAndFotDeleted_BazAndFotDummyValues() {
     Use-Tool -ToolName test -ToolSpec 'delete-test'
 
     ## Assert
+    Assert-That -ActualValue $env:path -Constraint { -not $env:BAZ }
+    Assert-That -ActualValue $env:path -Constraint { -not $env:FOT }
+}
+
+function Test.Use-Tool_PathAlteredAndFooSet_FooNotSetPathSetDummy() {
+    # Save the path
+    $PathBeforeTest = $env:path
+
+    ## Arrange
+    Set-PsEnvConfig .\PsEnvTools.json
+    $env:FOO = $null
+
+    ## Act
+    Use-Tool -ToolName 'batch-test'
+
+    ## Assert
     Assert-That -ActualValue $env:path -Constraint {
-        ((-not $env:BAZ) -and (-not $env:FOT))
+        $ActualValue -eq ".;..;$PathBeforeTest"
     }
+    Assert-That -ActualValue $env:FOO -Constraint {
+        $ActualValue -eq "Hello, World!"
+    }
+
+    # Restore path
+    $env:path = $PathBeforeTest
 }
---
+++ Test/legacy.bat
@@ -0,0 +1,2 @@
+set "PATH=.;..;%PATH%"
+set "FOO=Hello, World!"
