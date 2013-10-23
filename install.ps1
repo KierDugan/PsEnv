dd5890f900d3071dc3b1d0c043ee289c0149ad2d
Kier Dugan <kjd1v07@ecs.soton.ac.uk>
2013-10-23 23:14:16 +0100

    Adding an intall script to print a configuration message after PsEnv is installed.

---
+++ install.ps1
@@ -0,0 +1,11 @@
+Write-Host @"
+Thank you for installing PsEnv!
+Don't forget to create a JSON environment file and update your profile with:
+
+    Set-PsEnvConfig <your environments>.json
+
+You can find your profile at:
+
+    $profile
+
+"@
