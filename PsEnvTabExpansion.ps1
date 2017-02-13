32d732a97d41594bc390d2812cbb754132885e8c
Takekazu Omi <takekazuomi@gmail.com>
2017-02-13 14:24:09 +0900

    done toolname and spec command line completion support

--- PsEnvTabExpansion.ps1
+++ PsEnvTabExpansion.ps1
@@ -1,32 +1,32 @@
 
 function script:GetEnvironmentToolNames() {
-    return Get-Member -InputObject $Global:PsEnvConfig -MemberType NoteProperty | ForEach-Object -MemberName 'Name'
+    return [array](Get-Member -InputObject $Global:PsEnvConfig -MemberType NoteProperty | ForEach-Object -MemberName 'Name')
 }
 
 function script:GetEnvironmentSpecNames() {
     Param($toolName)
-#    Write-Host "GetEnvironmentSpecNames ", $toolName
-    return $Global:PsEnvConfig.$toolName | ForEach-Object{@{Name=$_.name; Display=$_.display}}
+    #    Write-Host "GetEnvironmentSpecNames ", $toolName
+    return $Global:PsEnvConfig.$toolName | ForEach-Object{$_.name}
 }
 
 function script:GetAliasPattern($command) {
-   $aliases = @($command) + @(Get-Alias | where { $_.Definition -eq $command } | select -Exp Name)
-   "($($aliases -join '|'))"
+    $aliases = @($command) + @(Get-Alias | Where-Object { $_.Definition -eq $command } | select -Exp Name)
+    "($($aliases -join '|'))"
 }
 
-
-
 function PsEnvTabExpansion($lastBlock) {
-    # Write-Host $lastBlock, $names
-    #Write-Host ("lastBlock: {0}-" -f $lastBlock)
+    $patten = "^(?<toolname>$((GetEnvironmentToolNames) -join '|'))\s+(?<toolspec>\S*)"
 
     switch -regex ($lastBlock -replace "^$(GetAliasPattern use-tool)\s+","") {
 
-        # Handles use <toolname> <toolspec>
-
+        # Handles git <cmd> <op>
+        "^(?<toolname>$((GetEnvironmentToolNames) -join '|'))\s+(?<toolspec>\S*)" {
+            (GetEnvironmentSpecNames $matches['toolname']) -like  ("{0}*" -f $matches['toolspec'])
+        }
+   
         # Handles use <toolname>
         "^(?<toolname>\S*)$" {
-            [array](GetEnvironmentToolNames) -like ("{0}*" -f $matches['toolname'])
+            (GetEnvironmentToolNames) -like ("{0}*" -f $matches['toolname'])
         }
     }
 }
@@ -36,22 +36,26 @@ if (Test-Path Function:\TabExpansion) {
 }
 
 function TabExpansion($line, $lastWord) {
-    Write-Host $line, $lastWord -ForegroundColor Red
+    # Write-Host $line, $lastWord -ForegroundColor Red
 
     $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
 
-    Write-Host "^$(GetAliasPattern use-tool) (.*)" -ForegroundColor Red
+    # Write-Host "^$(GetAliasPattern use-tool) (.*)" -ForegroundColor Red
 
-    Write-Host ("*{0}*" -f $lastBlock) -ForegroundColor Red
+    # Write-Host ("*{0}*" -f $lastBlock) -ForegroundColor Red
     switch -regex ($lastBlock) {
 
         # Execute psenv tab completion for all use-tool commands
         "^$(GetAliasPattern use-tool) (.*)" { 
-            Write-Host "matchd" -ForegroundColor Red
+            # Write-Host "matchd" -ForegroundColor Red
         
             PsEnvTabExpansion $lastBlock 
         }
         # Fall back on existing tab expansion
-        default { if (Test-Path Function:\TabExpansionBackup) { TabExpansionBackup $line $lastWord } }
+        default {
+            if (Test-Path Function:\TabExpansionBackup) {
+                TabExpansionBackup $line $lastWord 
+            } 
+        }
     }
 }
